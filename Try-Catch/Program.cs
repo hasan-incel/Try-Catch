a60220f5fbed219c4e1f8d5ddc0d62f2de829d1b
Hasan İncel <h_incel@hotmail.com>
2024-08-18 18:21:20 +0300

    Add project files.

---
+++ Try-Catch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Bir Sayı Giriniz");
+            double sayi = Convert.ToDouble(Console.ReadLine());
+            double kare = Math.Pow(sayi, 2);//sayinin karesi alınıyor.
+            Console.WriteLine("Girdiğiniz Sayının Karesi: " + kare);
+        }
+        catch (FormatException)
+        {
+            // Giriş geçersizse hata mesajını göster
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+        finally
+        {
+            Console.WriteLine("İşlem Tamamlandı");
+        }
+    }
+}
